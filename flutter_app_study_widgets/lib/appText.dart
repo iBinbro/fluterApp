c2a66013b42ef87144494d96b1a113d6c9ddc8c8
shutuiOS <>
2021-05-21 16:43:35 +0800

    富文本换行方式；

--- flutter_app_study_widgets/lib/appText.dart
+++ flutter_app_study_widgets/lib/appText.dart
@@ -8,33 +8,52 @@ class AppText extends StatelessWidget {
       appBar: AppBar(
         title: Text("富文本"),
       ),
-      body: Text.rich(
-        TextSpan(
-            text: "市场价格：",
-            style: TextStyle(color: Colors.blue, fontSize: 20),
-            //TextSpan 的style会影响到children 如果children没有style则延续父style
-            children: [
-              //放入一个widget和文本融合
-              WidgetSpan(
-                  child: Icon(
-                Icons.wrong_location_sharp,
-                size: 30,
-              )),
-              TextSpan(
-                text: "199",
-                style: TextStyle(color: Colors.red, fontSize: 30),
-                children: [
-                  TextSpan(
-                    text: "https://www.baidu.com",
-                    //添加手势
-                    recognizer: TapGestureRecognizer()..onTap = () {
-                      print("富文本点击事件");
-                    },
-                  )
-                ],
-              )
-            ]),
-        maxLines: 2,
+      body: Container(
+        width: 100,
+        height: 100,
+        child: Text.rich(
+          TextSpan(
+              text: "市场价格：",
+              style: TextStyle(color: Colors.blue, fontSize: 20),
+              //TextSpan 的style会影响到children 如果children没有style则延续父style
+              children: [
+                TextSpan(
+                  text: "199",
+                  style: TextStyle(color: Colors.red, fontSize: 30),
+                  children: [
+                    TextSpan(
+                      text:
+                          "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
+                      //添加手势
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          print("富文本点击事件");
+                        },
+                    )
+                  ],
+                ),
+                //放入一个widget和文本融合
+                WidgetSpan(
+                    child: Icon(
+                  Icons.wrong_location_sharp,
+                  size: 30,
+                )),
+              ]),
+
+          // clip,
+          // 裁剪掉超出部分.
+
+          // fade,
+          // 超出部分透明.
+
+          // ellipsis,
+          // 省略号.
+
+          // visible,
+          // 可见.
+          overflow: TextOverflow.ellipsis,
+          maxLines: 3,
+        ),
       ),
     );
   }
