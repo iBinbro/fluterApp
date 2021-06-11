dc2a10bd659b38497f6cc67e6e97a2ba7c85361b
shutuiOS <>
2021-06-11 17:45:58 +0800

    put的tag和GetBuilder的tag可同时对应一个controller；

--- flutter_app_study_widgets/lib/appGetXDemos_ShareControllerThroughRoute.dart
+++ flutter_app_study_widgets/lib/appGetXDemos_ShareControllerThroughRoute.dart
@@ -30,7 +30,9 @@ class GetXShareController extends StatelessWidget {
     print("GetXShareController data = ${this.data}");
 
     DataController dataController = DataController(this.data);
-    // Get.put(dataController);
+    //理解成一个栈一样 存放对应键值对 {tag : controller} 多个tag可对应同一个controller
+    // 比如在商品详情页 A商品详情 => B商品详情 => A商品详情 如果使用商品id作为controller，那是不是可以达到共享效果
+    Get.put(dataController, tag: "123");//类似用键值对的形式存储controller
 
     return Scaffold(
       appBar: AppBar(
@@ -41,7 +43,9 @@ class GetXShareController extends StatelessWidget {
           children: [
             GetBuilder<DataController>(
                 init: dataController, //传入tag 且tag会指定到这个controller上
-                tag: this.data.toString(), //可通过这个tag找到对应的controller
+                tag: "321",
+                // tag: this.data.toString(), //可通过这个tag找到对应的controller => Get.put(dataController,tag: "200");
+
                 //❌❌❌❌❌❌❌❌controller该controller是通过 Get查找而来 并不是init:传入的controller❌❌❌❌❌
                 builder: (controller) {
                   print(
@@ -59,7 +63,7 @@ class GetXShareController extends StatelessWidget {
                       TextButton(
                         onPressed: () {
                           print(
-                              "data == ${Get.find<DataController>(tag: "200").data}");
+                              "data == ${Get.find<DataController>(tag: "321").data}");
                         },
                         child: Text("获取DataController data"),
                       )
